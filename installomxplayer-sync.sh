ed1b231106acadf8ee9c63837c9e671cdaf68398
carlitoselmago <ccarbonell@gmail.com>
2022-01-23 14:29:02 +0100

    Update installomxplayer-sync.sh

--- installomxplayer-sync.sh
+++ installomxplayer-sync.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/sh
 #sudo su
-sudo apt-get remove omxplayer
-rm -rf /usr/bin/omxplayer /usr/bin/omxplayer.bin /usr/lib/omxplayer
+#sudo apt-get remove omxplayer
+#rm -rf /usr/bin/omxplayer /usr/bin/omxplayer.bin /usr/lib/omxplayer
 sudo apt-get install libpcre3 fonts-freefont-ttf fbset libssh-4 python3-dbus
-wget https://github.com/magdesign/PocketVJ-CP-v3/raw/master/sync/omxplayer_0.3.7-git20170130-62fb580_armhf.deb
-sudo dpkg -i omxplayer_0.3.7~git20170130~62fb580_armhf.deb
+#wget https://github.com/magdesign/PocketVJ-CP-v3/raw/master/sync/omxplayer_0.3.7-git20170130-62fb580_armhf.deb
+#sudo dpkg -i omxplayer_0.3.7~git20170130~62fb580_armhf.deb
 #sudo apt install omxplayer
 wget -O /usr/bin/omxplayer-sync https://github.com/turingmachine/omxplayer-sync/raw/master/omxplayer-sync
 chmod 0755 /usr/bin/omxplayer-sync
