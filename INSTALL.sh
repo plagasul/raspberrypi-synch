eed5a155f5876c8345747cdd000deb2251221e01
carlitoselmago <ccarbonell@gmail.com>
2022-02-03 12:55:12 +0100

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,7 +2,9 @@
 
 # RUN AS SU
 #install usbmount (automount usb drives)
+sudo sed -i "s/\(PrivateMounts *= *\).*/\1no/" /lib/systemd/system/systemd-udevd.service
 apt install usbmount
+
 
 #copy rc.local
 cp etc/rc.local /etc/rc.local
