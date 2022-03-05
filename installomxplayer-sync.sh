72a120f024bba4ad38254038960d79e940852377
Plagasul <plagasul@gmail.com>
2022-03-05 19:32:39 +0100

    Some changes after testing in a real rpi

--- installomxplayer-sync.sh
+++ installomxplayer-sync.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/sh
-#sudo su
 #sudo apt-get remove omxplayer
 #rm -rf /usr/bin/omxplayer /usr/bin/omxplayer.bin /usr/lib/omxplayer
-sudo apt-get install libpcre3 fonts-freefont-ttf fbset libssh-4 python3-dbus
+sudo apt-get install fonts-freefont-ttf libssh-4 python3-dbus
 #wget https://github.com/magdesign/PocketVJ-CP-v3/raw/master/sync/omxplayer_0.3.7-git20170130-62fb580_armhf.deb
 #sudo dpkg -i omxplayer_0.3.7~git20170130~62fb580_armhf.deb
-#sudo apt install omxplayer
-wget -O /usr/bin/omxplayer-sync https://github.com/turingmachine/omxplayer-sync/raw/master/omxplayer-sync
-chmod 0755 /usr/bin/omxplayer-sync
+sudo apt install omxplayer
+sudo wget -O /usr/bin/omxplayer-sync https://github.com/turingmachine/omxplayer-sync/raw/master/omxplayer-sync
+sudo chmod 0755 /usr/bin/omxplayer-sync
 
 # Download twitch video for sync testing
 wget http://twitch-event-engineering-public.s3.amazonaws.com/sync-footage/Sync-Footage-V1-H264.mp4
