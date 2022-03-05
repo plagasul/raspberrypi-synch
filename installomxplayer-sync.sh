a9cd40c9c2aaa5cd54f26f2f0236744adefcbd19
Plagasul <plagasul@gmail.com>
2022-03-05 18:11:12 +0100

    Rename sync test video so it plays right away

--- installomxplayer-sync.sh
+++ installomxplayer-sync.sh
@@ -8,4 +8,9 @@ sudo apt-get install libpcre3 fonts-freefont-ttf fbset libssh-4 python3-dbus
 #sudo apt install omxplayer
 wget -O /usr/bin/omxplayer-sync https://github.com/turingmachine/omxplayer-sync/raw/master/omxplayer-sync
 chmod 0755 /usr/bin/omxplayer-sync
+
+# Download twitch video for sync testing
 wget http://twitch-event-engineering-public.s3.amazonaws.com/sync-footage/Sync-Footage-V1-H264.mp4
+
+# I guess we need to rename it to video.mp4 so it works straightaway with line 39 of rc.local
+mv Sync-Footage-V1-H264.mp4 video.mp4
