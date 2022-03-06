fac7b0fe13cfa85e3fb365e5647f85264c96600f
Plagasul <plagasul@gmail.com>
2022-03-06 12:00:52 +0100

    Comments, comments everywhere

--- installomxplayer-sync.sh
+++ installomxplayer-sync.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/sh
-#sudo apt-get remove omxplayer
-#rm -rf /usr/bin/omxplayer /usr/bin/omxplayer.bin /usr/lib/omxplayer
+
+# Some dependencies
 sudo apt-get install fonts-freefont-ttf libssh-4 python3-dbus
-#wget https://github.com/magdesign/PocketVJ-CP-v3/raw/master/sync/omxplayer_0.3.7-git20170130-62fb580_armhf.deb
-#sudo dpkg -i omxplayer_0.3.7~git20170130~62fb580_armhf.deb
+
+# Use newest version of omxplayer in the repo, should work
 sudo apt install omxplayer
+
+# Get omxplayer-sync into /bin and make it executable
 sudo wget -O /usr/bin/omxplayer-sync https://github.com/turingmachine/omxplayer-sync/raw/master/omxplayer-sync
 sudo chmod 0755 /usr/bin/omxplayer-sync
 
 # Download twitch video for sync testing
 wget http://twitch-event-engineering-public.s3.amazonaws.com/sync-footage/Sync-Footage-V1-H264.mp4
 
-# Assuming this is a cloned repo, we rename the video and move it to ~
+# Assuming we are in a subfolder of ~ (for example if we cloned the repo) we rename the video and move it up to ~
 mv Sync-Footage-V1-H264.mp4 ../video.mp4
